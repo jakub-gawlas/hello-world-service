f3040dc779257474230459f998086b2dee12d9c1
Gawlas, Jakub <jakub.gawlas@sap.com>
2016-11-23 22:46:13 +0100

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import Koa from 'koa';
+import Router from 'koa-router';
+
+const app = new Koa();
+const router = new Router();
+
+
+router.get('/', (ctx, next) => {
+  ctx.body = {
+    message: 'Hello World!'
+  };
+});
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Service started on *:${PORT}`));
